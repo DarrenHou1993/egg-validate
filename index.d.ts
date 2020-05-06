98a915e6434ad9102946ff8a490e30a05c14c71b
侯栋樑 <houdl@mail.open.com.cn>
2020-05-06 21:21:03 +0800

    更新parameter版本

--- index.d.ts
+++ index.d.ts
@@ -11,12 +11,25 @@ interface ValidateError {
 declare module 'egg' {
   export interface Application {
     validator: {
-      addRule: (type: string, check: RegExp | CheckHandlerFunc) => void;
+      addRule: (
+        type: string,
+        check: RegExp | CheckHandlerFunc,
+        override?: boolean,
+        convertType?: Function | String
+      ) => void;
       validate: (rules: any, data: any) => ValidateError[];
     };
   }
 
   export interface Context {
     validate: (rules: any, data?: any) => void;
+  }
+  interface EggAppConfig {
+    validate: {
+      translate: Function;
+      validateRoot: boolean;
+      convert: boolean;
+      widelyUndefined: boolean;
+    };
   }
 }
